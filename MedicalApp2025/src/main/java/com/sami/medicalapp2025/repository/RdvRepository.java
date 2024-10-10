699018d8c2ba166473367ee02f4c38b7acc31e8a
sami <samiayachi653@gmail.com>
2024-10-10 20:51:11 +0100

    fix :bug:

--- MedicalApp2025/src/main/java/com/sami/medicalapp2025/repository/RdvRepository.java
+++ MedicalApp2025/src/main/java/com/sami/medicalapp2025/repository/RdvRepository.java
@@ -3,5 +3,9 @@ package com.sami.medicalapp2025.repository;
 import com.sami.medicalapp2025.entities.Rdv;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
+
 public interface RdvRepository extends JpaRepository<Rdv,Integer> {
+    Rdv findByPatientIdAndDateRdv(int id, LocalDateTime ldt);
+    Rdv findByMedecinIdAndDateRdv(int id,LocalDateTime ldt);
 }
